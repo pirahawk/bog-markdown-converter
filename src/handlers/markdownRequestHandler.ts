256ee37da037adf5d8858f0665769ea2cd14e98a
pirahawk <piran.tata@gmail.com>
2019-06-05 22:50:32 +0100

    updated handler to render markdown simple for now

--- src/handlers/markdownRequestHandler.ts
+++ src/handlers/markdownRequestHandler.ts
@@ -1,9 +1,18 @@
 import { IHttpRouteHandler } from "../iHttpRouteHandler";
 import {Request, Response} from 'express';
+import MarkdownIt = require("markdown-it");
+import {Options, Rule} from 'markdown-it';
 
 export class MarkdownRequestHandler implements IHttpRouteHandler {
     handle(request: Request, response: Response): void {
+        if(!request.body){
+            response.status(400).send();
+            return;
+        }
 
-        response.send('hello');
+        let parser: MarkdownIt = new MarkdownIt();
+        let renderedResponse = parser.render(request.body);
+
+        response.send(renderedResponse);
     }
 }
