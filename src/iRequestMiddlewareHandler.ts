cd14e1fb65ceb5924a0211bc4a13066aecf36e71
pirahawk <piran.tata@gmail.com>
2020-04-07 00:15:39 +0100

    im rebuilding this entire thing!

--- src/iRequestMiddlewareHandler.ts
+++
@@ -1,6 +0,0 @@
-import {Request, Response, NextFunction} from 'express';
-
-
-export interface IRequestMiddlewareHandler{
-    handle(request: Request, response: Response, next: NextFunction): void;
-}
