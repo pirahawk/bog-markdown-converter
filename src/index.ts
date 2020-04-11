358ed257a9e313d9a87d57abba3308f36f9d9e6a
pirahawk <piran.tata@gmail.com>
2020-04-11 16:04:12 +0100

    Add env variables to the vscode launch json

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,14 @@
 
 import { ServerConfiguration } from "./core/ServerConfiguration";
 import { ServerRunner } from "./core/ServerRunner";
+import { parse } from "querystring";
 
+
+
+let expressPort:number = parseInt(process.env.EXPRESS_PORT as string);
 let config:ServerConfiguration = {
-    port: 3000
+    port: expressPort
 };
-
-// let express = require('express');
-// let app = express();
-
-// app.listen(config.port, ()=> console.log(`App has Started!!!`));
 
 let serverRunner = new ServerRunner();
 serverRunner.run(config);
